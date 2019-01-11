aacb90ced396e706b421e992609eb725debba1bb
ookiiani <jakestr1999@gmail.com>
2019-01-11 19:40:21 +0000

    Smaller form

--- AccessTest/Form1.Designer.cs
+++ AccessTest/Form1.Designer.cs
@@ -147,7 +147,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1358, 474);
+            this.ClientSize = new System.Drawing.Size(870, 474);
             this.Controls.Add(this.testGrid);
             this.Controls.Add(this.insertQueryBtn);
             this.Controls.Add(this.selectQueryBtn);
